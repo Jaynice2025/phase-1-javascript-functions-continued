ddcbfd0d3bd5c239aeddd3b30510728f5cd4aff4
Jaynice <janyce.wathuo@student.moringaschool.com>
2025-03-20 11:12:02 +0300

    coded into index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+// 1. Function saturdayFun
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // 2. Function mondayWork
+  function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  // 3. Function wrapAdjective
+  function wrapAdjective(wrapper = "*") {
+    return function(adjective = "special") {
+      return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+  }
+  
+  // Example usage:
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction("a dedicated programmer")); // You are !!!a dedicated programmer!!!
+  
